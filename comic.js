be80d4e04a1d0ba80bfe86a07546379b5639d17f
Caitlin <c_smith24@mail.fhsu.edu>
2024-10-07 17:23:39 -0500

    initial commit

---
+++ comic.js
@@ -0,0 +1,23 @@
+function btn1() {
+	window.open('avengers.html', '_blank')
+}
+
+function btn2() {
+	window.open('spiderman.html', '_blank')
+}
+
+function btn3() {
+	window.open('strange.html', '_blank')
+}
+
+function btn4() {
+	window.open('batman.html', '_blank')
+}
+
+function btn5() {
+	window.open('captain.html', '_blank')
+}
+
+function btn6() {
+	window.open('ironman.html', '_blank')
+}
